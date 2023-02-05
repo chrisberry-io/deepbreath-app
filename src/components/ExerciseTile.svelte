5bb5993207bdb047eb7e4485bc582e69a0164a99
Chris Berry <chrisberry08@gmail.com>
2023-02-05 07:06:37 -0500

    feat: card styles

--- src/components/ExerciseTile.svelte
+++ src/components/ExerciseTile.svelte
@@ -8,13 +8,33 @@
 </script>
 
 <li class="tile">
-	<a href={link}>{name}</a>
-	<UpDown value={reps} />
+	<div class="left">
+		<a href={link}>{name}</a>
+		<UpDown value={reps} />
+	</div>
+	<slot name="icon" />
 </li>
 
-<style>
+<style lang="scss">
 	.tile {
 		margin: 0;
 		padding: 0;
+		background: var(--color-surface1);
+		display: flex;
+		border-radius: 1.5rem;
+		padding: 1.75rem;
+		gap: 1.5rem;
+		.left {
+			flex: 1;
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+			a {
+				font-size: 1.5rem;
+				font-weight: 500;
+				color: var(--color-text1);
+				text-decoration: none;
+			}
+		}
 	}
 </style>
