39f4c0a3ee4d2201bdf6a82fecab1e99a448827d
chrisberry-io <chrisberry08@gmail.com>
2023-02-07 22:02:16 -0500

    Revert "feat: added web vitals"

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,18 +2,6 @@
 	import '$lib/assets/scss/global.scss';
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
-	import { webVitals } from '$lib/vitals';
-	let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
-	import { browser } from '$app/environment';
-	import { page } from '$app/stores';
-
-	$: if (browser && analyticsId) {
-		webVitals({
-			path: $page.url.pathname,
-			params: $page.params,
-			analyticsId
-		});
-	}
 
 	inject({ mode: dev ? 'development' : 'production' });
 </script>
